4f641da74dca04818601156f3e80f38b1f751c63
Cedric <stormyz22a@gmail.com>
2026-02-25 14:33:16 +0300

    new codes

---
+++ lesson1c.js
@@ -0,0 +1,9 @@
+// 1. Declaring variables using let
+let height = 180       
+let temperature = 36.5  
+let points = 95         
+
+// 2. Printing the variables using console.log
+console.log(height)
+console.log(temperature)
+console.log(points)
---
+++ lesson1d.js
@@ -0,0 +1,26 @@
+//  String data type
+// A string is a series/sequence of character that are enclosed inside of quotation marks
+let name = "Cedric Kipngeno"
+console.log("The name is: ", name)
+
+// floating point: This is a number with decimal
+let quantity = 35.5
+console.log("The quantity of sugar bought is: ",quantity)
+
+// Boolean : It returns true or false
+let isAdult = true
+let isRegistered = false
+console.log("Is Cedric an Adult? ", isAdult)
+console.log("Is he registered as a voter? ", isRegistered)
+
+console.log(typeof(isAdult))
+
+// undefined : This is a data type that has been declared but there is no value that has been assigned to it.
+let county;
+console.log("The county is: ", county)
+
+// null data type : It is a data type that contains null values
+let username = null
+console.log("What is the username?: ", username)
+
+// Assignment research and come up with examples on Object and Array data types. Show how to access items of the object and of any array
